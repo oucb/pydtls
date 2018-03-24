af761b50cb97cc912a60950754f10dd0ccd09c77
oucb <oucb@outlook.com>
2018-03-24 17:30:38 +0800

    fix(openssl.py): modify the library loading to support macOS

--- dtls/openssl.py
+++ dtls/openssl.py
@@ -64,6 +64,9 @@ if sys.platform.startswith('win'):
     ssldll_path = path.join(dll_path, "ssleay32.dll")
     libcrypto = CDLL(cryptodll_path)
     libssl = CDLL(ssldll_path)
+elif sys.platform == 'darwin':
+    libcrypto = CDLL("libcrypto.1.0.0.dylib")
+    libssl = CDLL("libssl.1.0.0.dylib")
 else:
     libcrypto = CDLL("libcrypto.so.1.0.0")
     libssl = CDLL("libssl.so.1.0.0")
